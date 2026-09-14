dad8ab2e89f38d597fe06deff4b8be348dd9a340
Zihan Fakir <zihanfakir@gmail.com>
2026-09-14 12:01:00 +0600

    Deep Optimization: R8 Minify Android App, Lazy Load Images, Auto Compress Camera Photos

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
