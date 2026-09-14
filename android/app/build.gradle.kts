61c7d8421bafac5c2c872754e9ae3ff50fde79a4
Zihan Fakir <zihanfakir@gmail.com>
2026-09-14 11:40:25 +0600

    Update Android App to Version 1.0.7

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "xyz.zihan.aloai"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
